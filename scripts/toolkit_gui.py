792be676c4e4e150c8df02ef5bba858f185a65be
yufengyao-lingoace <fengyao.yu@pplingo.com>
2023-02-15 12:25:51 +0800

    yu

--- scripts/toolkit_gui.py
+++ scripts/toolkit_gui.py
@@ -667,7 +667,7 @@ def on_ui_tabs():
         with gr.Row() as load_row:
             tabname=""
             # gr.Image(source="")
-            init_img_inpaint = gr.Image(type="pil")
+            init_img_inpaint = gr.Image(value="data/img/e1a8eeba-760d-4528-a3ea-34e578bcb725.jpg",type="pil")
             # result_gallery = gr.Gallery(label='Output', show_label=False, elem_id=f"{tabname}_gallery").style(grid=4)
             # image = gr.Image(elem_id="pnginfo_image", label="Source", source="upload", interactive=True, type="pil")
             # img=gr.Image(type="pil") #value="data/img/e1a8eeba-760d-4528-a3ea-34e578bcb725.jpg"
