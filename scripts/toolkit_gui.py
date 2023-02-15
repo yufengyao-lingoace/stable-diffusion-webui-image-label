16b8de35d9797184c5304027d947aac4761b8c67
yufengyao-lingoace <fengyao.yu@pplingo.com>
2023-02-15 16:40:33 +0800

    1

--- scripts/toolkit_gui.py
+++ scripts/toolkit_gui.py
@@ -8,16 +8,16 @@ import gc
 import cv2
 from toolkit import *
 
-data_folder="/data/stable-diffusion-webui/extensions/stable-diffusion-webui-image-label/data"
-label_folder="/data/stable-diffusion-webui/extensions/stable-diffusion-webui-image-label/data/img"
-label_images=[]
+data_folder = "/data/stable-diffusion-webui/extensions/stable-diffusion-webui-image-label/data"
+label_folder = "/data/stable-diffusion-webui/extensions/stable-diffusion-webui-image-label/data/img"
+label_images = []
 
 
 def do_clear():
     global loaded
     loaded = None
     gc.collect()
-    
+
     reports = [gr.update(value=""), gr.update(value="")]
     sources = [gr.update(), gr.update()]
     drops = [gr.update(choices=[], value="") for _ in range(3)]
@@ -28,15 +28,18 @@ def do_clear():
     updates = reports + sources + drops + rows + names + error
     return updates
 
+
 def do_save(prompt):
-    img_file=label_images.pop()
-    img_file=None if img_file=="" else img_file
+    img_file = label_images.pop()
+    img_file = None if img_file == "" else img_file
     return img_file
 
+
 def do_load(prompt):
-    img_file=label_images.pop()
-    img_file=None if img_file=="" else img_file
+    img_file = label_images.pop()
+    img_file = None if img_file == "" else img_file
     return img_file
+
 
 def on_ui_tabs():
     global label_images
@@ -47,9 +50,10 @@ def on_ui_tabs():
         .dark .gr-compact { margin-left: unset } #image {height:30em;} #save_button {height:6.5em;} #load_button{height:3.7em;}
         #errormd { min-height: 0rem; text-align: center; } #errormd h3 { color: #ba0000; }
     """
-    root,dirs,files = os.walk(data_folder)[0]
-    files=os.listdir(label_folder)
-    label_images=[os.path.join(label_folder,f) for f in files]
+    root, dirs, files = os.walk(data_folder)
+    print(os.walk(data_folder))
+    files = os.listdir(label_folder)
+    label_images = [os.path.join(label_folder, f) for f in files]
 
     with gr.Blocks(css=css, analytics_enabled=False, variant="compact") as image_label:
         gr.HTML(value=f"<style>{css}</style>")
@@ -57,25 +61,27 @@ def on_ui_tabs():
             with gr.Column(scale=4):
                 with gr.Row():
                     comp_dropdown = gr.Dropdown(label="Dataset", choices=dirs, interactive=True)
-                    user_dropdown = gr.Dropdown(label="User Name", choices=['001','002','003','004','005','006','007','008','009','010'], interactive=True)
+                    user_dropdown = gr.Dropdown(label="User Name", choices=['001', '002', '003', '004', '005', '006', '007', '008', '009', '010'], interactive=True)
             with gr.Column(scale=1):
-                load_button = gr.Button(value="Load", variant="primary",elem_id="load_button")
-                
+                load_button = gr.Button(value="Load", variant="primary", elem_id="load_button")
+
         with gr.Row() as load_row:
             img = gr.Image(elem_id="image")
         with gr.Row():
             with gr.Column(scale=4):
                 prompt = gr.Textbox(label="Prompt", elem_id="txt_prompt", show_label=False, lines=3, placeholder="Prompt (press Ctrl+Enter or Alt+Enter to save and jump to next)")
             with gr.Column(scale=1):
-                save_button = gr.Button(value='Next', variant="primary",elem_id="save_button")
+                save_button = gr.Button(value='Next', variant="primary", elem_id="save_button")
         save_button.click(fn=do_save, inputs=prompt, outputs=img)
         load_button.click(fn=do_load, inputs=None, outputs=img)
 
     return (image_label, "Label", "image_label"),
 
+
 def on_ui_settings():
     section = ('image-label', "Image Label")
     shared.opts.add_option("model_toolkit_fix_clip", shared.OptionInfo(False, "Fix broken CLIP position IDs", section=section))
+
 
 script_callbacks.on_ui_settings(on_ui_settings)
 script_callbacks.on_ui_tabs(on_ui_tabs)
