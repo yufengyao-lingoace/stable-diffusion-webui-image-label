712bb94fe9111cc696e0fbfd8674742e6e43269e
yufengyao-lingoace <fengyao.yu@pplingo.com>
2023-02-16 09:55:26 +0800

    1

--- scripts/toolkit_gui.py
+++ scripts/toolkit_gui.py
@@ -42,7 +42,7 @@ def on_ui_tabs():
         .float-text { float: left; } .float-text-p { float: left; line-height: 2.5rem; } #mediumbutton { max-width: 32rem; } #smalldropdown { max-width: 2rem; } #smallbutton { max-width: 2rem; }
         #toolbutton { max-width: 8em; } #toolsettings > div > div { padding: 0; } #toolsettings { gap: 0.4em; } #toolsettings > div { border: none; background: none; gap: 0.5em; }
         #reportmd { padding: 1rem; } .dark #reportmd thead { color: #daddd8 } .gr-prose hr { margin-bottom: 0.5rem } #reportmd ul { margin-top: 0rem; margin-bottom: 0rem; } #reportmd li { margin-top: 0rem; margin-bottom: 0rem; }
-        .dark .gr-compact { margin-left: unset } #image {height:30em;} #next_button {height:6.5em;} #load_button{height:3.7em;}
+        .dark .gr-compact { margin-left: unset } #image {height:30em;} #next_button,#previous_button {height:6.5em;} #load_button{height:3.7em;}
         #errormd { min-height: 0rem; text-align: center; } #errormd h3 { color: #ba0000; }
     """
     for root, dirs, files in os.walk(data_folder):
@@ -65,7 +65,7 @@ def on_ui_tabs():
         with gr.Row() as load_row:
             image = gr.Image(elem_id="image",type="pil")
         with gr.Row():
-            with gr.Column(scale=4):
+            with gr.Column(scale=6):
                 prompt = gr.Textbox(label="Prompt", elem_id="txt_prompt", show_label=False, lines=3, placeholder="Prompt (press Enter to save and jump to next)")
             with gr.Column(scale=1):
                 next_button = gr.Button(value='Next', variant="primary", elem_id="next_button")
