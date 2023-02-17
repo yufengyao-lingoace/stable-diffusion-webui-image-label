9da08cf14fa92afea42307d08396e57aa42bcba6
yufengyao-lingoace <fengyao.yu@pplingo.com>
2023-02-17 14:00:03 +0800

    1

--- scripts/toolkit_gui.py
+++ scripts/toolkit_gui.py
@@ -51,7 +51,9 @@ def do_save(file_name,prompt,dataset_name,user_name):
             writer.write(file_name+'\r\n')
         img_file = data_sets[dataset_name].pop()
         prompt_txt=""
-    history[user_name]["data"].append("{0}/{1}".format(dataset_name,file_name))#保存历史
+    history_item="{0}/{1}".format(dataset_name,file_name)
+    if history_item not in history[user_name]["data"]:
+        history[user_name]["data"].append(history_item)#保存历史
     history[user_name]["index"]+=1
 
     return img_file,os.path.basename(img_file),prompt_txt
@@ -75,7 +77,9 @@ def do_pass(file_name,dataset_name,user_name): #下一个
             writer.write(file_name+'\r\n')
         img_file = data_sets[dataset_name].pop()
         prompt_txt=""
-    history[user_name]["data"].append("{0}/{1}".format(dataset_name,file_name))#保存历史
+    history_item="{0}/{1}".format(dataset_name,file_name)
+    if history_item not in history[user_name]["data"]:
+        history[user_name]["data"].append(history_item)#保存历史
     history[user_name]["index"]+=1
     return img_file,os.path.basename(img_file) ,prompt_txt
 
